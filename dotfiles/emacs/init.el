3ad9dacf1f8bf909a533b3931423ab25598359cb
Your Name <you@example.com>
2024-11-01 19:00:18 +0200

    clear up package content and signatures

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -9,7 +9,7 @@
 ;; UI Settings
 ;; ============================
 
-(set-fringe-mode 10)
+;;(set-fringe-mode 10)
 
 (setq visible-bell t)
 
@@ -34,10 +34,24 @@
 ;; ============================
 ;; Package Management
 ;; ============================
-
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/") 
+			 ("org" . "https://orgmode.org/elpa/") 
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+;;        ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+
+
+
+(setq package-check-signature nil)
+
 (package-initialize)
+
+(package-refresh-contents)
+
+(package-install 'use-package)
+
+;;(unless package-archive-contents (package-refresh-contents))
 
 ;; Install use-package if not already installed
 (unless (package-installed-p 'use-package) 
@@ -46,59 +60,25 @@
 
 (require 'use-package)
 
-;; ============================
-;; Which Key Setup
-;; ============================
+(setq use-package-always-ensure t)
 
-(use-package 
-  which-key 
-  :ensure t 
-  :config (setq which-key-idle-delay 0.5) ;; Show after 0.5 seconds
-  (which-key-mode))			  ;; Enable which-key mode
 
-;; ============================
-;; Rust Development Setup
-;; ============================
+;; ===========================
+;; Ivy
+;; ===========================
 
-(use-package 
-  rust-mode 
-  :hook (rust-mode . lsp) ;; Enable LSP when rust-mode is active
-  :config (setq rust-format-on-save t))	;; Format on save
+;; Refresh package list if `ivy` is not already installed
+(unless (package-installed-p 'ivy) 
+  (package-refresh-contents) 
+  (package-install 'ivy))
 
-(use-package 
-  lsp-mode 
-  :commands lsp 
-  :config (setq lsp-prefer-flymake nil)	;; Use flycheck instead of flymake
-  (setq lsp-rust-analyzer-cargo-watch-command "clippy")) ;; Use clippy for diagnostics
-
-(use-package 
-  lsp-ui 
-  :commands lsp-ui-mode 
-  :config (setq lsp-ui-doc-enable t)) ;; Enable documentation popup
-
-(use-package 
-  company 
-  :hook (after-init . global-company-mode) 
-  :config (setq company-minimum-prefix-length 1 company-idle-delay 0.0)) ;; Show completion suggestions immediately
-
-;; ============================
-;; Flycheck Setup
-;; ============================
-
-(use-package 
-  flycheck 
-  :ensure t 
-  :init (global-flycheck-mode))	;; Enable Flycheck globally
-
-;; Ensure Flycheck integrates with LSP
-(setq lsp-diagnostic-package 
-      :flycheck)
+(ivy-mode 1)
 
 ;; ============================
 ;; Key Bindings for Navigation
 ;; ============================
 
-(require 'which-key)
+;;(require 'which-key)
 
 (global-set-key (kbd "C-x C-f") 'find-file)	  ;; Open file
 (global-set-key (kbd "M-<") 'beginning-of-buffer) ;; Go to start of file
@@ -106,7 +86,7 @@
 (global-set-key (kbd "C-x b") 'switch-to-buffer)  ;; Switch buffers
 
 ;; Set Space as leader key
-(global-set-key (kbd "SPC") 'which-key-show-top-level)
+;;(global-set-key (kbd "SPC") 'which-key-show-top-level)
 
 ;; ============================
 ;; Auto-formatting Configuration
@@ -124,9 +104,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ispell-dictionary nil) 
- '(package-selected-packages '(which-key cmake-mode use-package rust-mode projectile elisp-format
-					 company)))
+ '(package-selected-packages '(elisp-format ivy use-package cmake-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
