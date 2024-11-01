d3dd90faaa8be62c8a957c6c640e7e583ae09ac6
Your Name <you@example.com>
2024-11-02 00:07:53 +0200

    custom file

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -19,15 +19,5 @@
 ;; Auto-format init.el file on save
 (add-hook 'emacs-lisp-mode-hook (lambda () 
 				  (add-hook 'before-save-hook 'elisp-format-buffer nil 'local)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(elisp-format ivy use-package cmake-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
