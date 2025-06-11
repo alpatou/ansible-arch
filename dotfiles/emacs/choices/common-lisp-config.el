a8fa92064b87cfacc9f295fdf55d8edffb8b6f23
Your Name <you@example.com>
2025-06-12 01:36:12 +0300

    evil and slime (gain)

--- dotfiles/emacs/choices/common-lisp-config.el
+++ dotfiles/emacs/choices/common-lisp-config.el
@@ -1,18 +1,7 @@
-(use-package sly
+(use-package slime
   :ensure t
   :after (package)
   :config
   ;; Configure your preferred Common Lisp implementation
   (setq inferior-lisp-program "/usr/bin/sbcl")  ; e.g., "/usr/local/bin/sbcl"
-
-  ;; Enable SLY mode for Lisp files
-  (add-hook 'lisp-mode-hook #'sly-mode)
-
-  ;; Start SLY when opening Lisp files
-  (setq sly-auto-start 'always))
-
-(use-package sly-quicklisp
-  :ensure t
-  :after (sly)
-  :config
-  (setq sly-contribs '(sly-quicklisp)))
+  (slime-setup '(slime-fancy)))
--- dotfiles/emacs/choices/general.el
+++ dotfiles/emacs/choices/general.el
@@ -3,7 +3,8 @@
 ;; ====================
 
 (setq inhibit-startup-message t)
-
+(use-package evil
+  :ensure t)
 ;; in case i want to delete the selected text on typing
 ;;(use-package delsel
 ;;  :ensure nil ; no need to install it as it is built-in
