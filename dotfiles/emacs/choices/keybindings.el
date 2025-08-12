d34f33855af30512c0311e991320aea540551dd6
Your Name <you@example.com>
2025-08-12 12:11:46 +0300

    mark command for macOS

--- dotfiles/emacs/choices/keybindings.el
+++ dotfiles/emacs/choices/keybindings.el
@@ -7,6 +7,8 @@
 (global-set-key (kbd "M->") 'end-of-buffer)	  ;; Go to end of file
 (global-set-key (kbd "C-x b") 'switch-to-buffer)  ;; Switch buffers
 
+(global-set-key (kbd "C-c 2") 'set-mark-command)
+
 ;; Enable directional window movement with Shift+arrow keys
 (windmove-default-keybindings)
 (global-set-key (kbd "C-c h") 'windmove-left)
