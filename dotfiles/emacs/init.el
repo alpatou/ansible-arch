e5cd473af349a6720f28ed26e2938bc0661fe897
Your Name <you@example.com>
2025-08-20 23:47:29 +0300

    links corectly on org

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -38,9 +38,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(company counsel-projectile dockerfile-mode elisp-format evil go-mode
+	     lsp-ui marginalia nord-theme orderless paredit php-mode
+	     projectile rainbow-delimiters rust-mode slime
+	     typescript-mode vertico vterm yaml-mode))
  '(package-vc-selected-packages
-   '((tsx-mode :url "https://github.com/orzechowskid/tsx-mode.el"))))
+   '((go-mode :url "https://github.com/dominikh/go-mode.el.git"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
