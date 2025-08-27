7d21c9d8b6f86ee0a945a2ae83cb39e2e6e68db2
Your Name <you@example.com>
2025-08-27 15:53:51 +0300

    cua mode rror

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -38,11 +38,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(company counsel-projectile dockerfile-mode elisp-format evil go-mode
-	     lsp-ui marginalia nord-theme orderless paredit php-mode
-	     projectile rainbow-delimiters rust-mode slime
-	     typescript-mode vertico vterm yaml-mode))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((go-mode :url "https://github.com/dominikh/go-mode.el.git"))))
 (custom-set-faces
