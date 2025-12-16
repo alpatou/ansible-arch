4f150362de7b206ab9c203a9ace2d80aece583df
Your Name <you@example.com>
2025-12-16 17:46:52 +0200

    * search like vim, and others

--- dotfiles/emacs/choices/keybindings.el
+++ dotfiles/emacs/choices/keybindings.el
@@ -15,3 +15,37 @@
 (global-set-key (kbd "C-c j") 'windmove-down)
 (global-set-key (kbd "C-c k") 'windmove-up)
 (global-set-key (kbd "C-c l") 'windmove-right)
+
+(defun toggle-evil-mode ()
+  "Toggle Evil mode and display a message."
+  (interactive)
+  (if (bound-and-true-p evil-mode)
+      (progn
+        (evil-mode 0)
+        (message "Evil mode disabled"))
+    (evil-mode 1)
+    (message "Evil mode enabled")))
+
+
+(defun isearch-word-at-point ()
+  "Start an incremental search forward with the word under cursor."
+  (interactive)
+  (let ((word (thing-at-point 'word t)))
+    (when word
+      (isearch-mode t)
+      (isearch-yank-string word))))
+
+;; Bind to * if not already used
+(global-set-key (kbd "C-c C-w") 'isearch-word-at-point)
+
+
+(defun isearch-symbol-at-point ()
+  "Start an incremental search forward with the symbol under cursor."
+  (interactive)
+  (let ((sym (thing-at-point 'symbol t)))
+    (when sym
+      (isearch-mode t)
+      (isearch-yank-string sym))))
+
+;; p for phrase
+(global-set-key (kbd "C-c C-p") 'isearch-symbol-at-point)
